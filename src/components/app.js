869939f8a23f538a5d8e61b577da7b4282befa79
MicahWeiss <micahjweiss@gmail.com>
2018-10-05 14:09:04 -0500

    got template visible

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,9 @@
-angular.module('video-player')
-
-.component('app', {
+angular.module('video-player').component('app', {
   // TODO
+  templateUrl: 'src/templates/app.html',
+  // selectVideo: function () {
+
+  // }
+  controller: 'appCtrl'
+
 });
